c30f1d4573a52537591da41a807f211df3a87b03
Nemanja Mijailovic <nemanja.mijailovic2912@gmail.com>
2023-10-20 00:52:18 +0200

    small changes

--- dist/assets/contact.module-9ced1550.js
+++
@@ -1,6 +0,0 @@
-const t = "_contact_1gzi0_1",
-  o = "_bgImage_1gzi0_5",
-  c = "_description_1gzi0_32",
-  n = "_buttonWrapper_1gzi0_40",
-  s = { contact: t, bgImage: o, description: c, buttonWrapper: n };
-export { s };
--- dist/assets/whiteCheckmark-b355e707.js
+++
@@ -1,2 +0,0 @@
-const e = "/nefentus/assets/whiteCheckmark-c6f064cb.svg";
-export { e as C };
