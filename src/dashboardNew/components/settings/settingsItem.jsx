8a49ad29e63ff6cae9bac375de7c72b0399b568d
Nazarii Melnyk <nazariimelnyk@outlook.com>
2023-10-22 20:02:52 +0300

    connect backend with profile pages

--- src/dashboardNew/components/settings/settingsItem.jsx
+++ src/dashboardNew/components/settings/settingsItem.jsx
@@ -4,32 +4,21 @@ import styles from "./settingsTitle.module.css";
 import Fail from "../../../assets/icon/fail.svg";
 import Correct from "../../../assets/icon/correct.svg";
 
-import { classNames } from "classnames";
 import { useEffect, useState } from "react";
-import Card from "../card/card";
 import Popup from "../popup/popup";
+import CropDialog, {
+  dataURLtoFile,
+} from "../../../components/cropDialog/cropDialog";
 
-const SettingsItem = ({ data }) => {
-  const [value, setValue] = useState([]);
+const SettingsItem = ({ data, setIsSaveData }) => {
   const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    if (value.length > 0) return;
-
-    const valueList = [];
-
-    for (let i = 0; i < data.list.length; i++) {
-      valueList.push(data.list[i].value);
-    }
-
-    setValue([...valueList]);
-  }, [value, data]);
+  const [cropDialogOpen, setCropDialogOpen] = useState(false);
 
   const handleEdit = () => {
     setShow(true);
   };
 
-  const handleChangeImage = (index) => {
+  const handleChangeImage = () => {
     const fileInput = document.createElement("input");
     fileInput.type = "file";
     fileInput.accept = "image/*";
@@ -40,81 +29,85 @@ const SettingsItem = ({ data }) => {
       if (selectedFile) {
         const imageURL = URL.createObjectURL(selectedFile);
 
-        setValue((prev) => {
-          prev[index] = imageURL;
-
-          return [...prev];
-        });
+        data.setValue(imageURL);
+        setCropDialogOpen(true);
+        data.setFile(selectedFile);
+        data.setImageChanged(true);
       }
     });
   };
 
-  const handleEnable = (index) => {
-    setValue((prev) => {
-      prev[index] = !prev[index];
-
-      return [...prev];
-    });
+  const handleEnable = () => {
+    data.setValue((prev) => !prev);
   };
 
-  const handleData = (data, index) => {
-    setValue((prev) => {
-      prev[index] = data;
-      return [...prev];
-    });
+  const handleData = (dataValue) => {
+    data.setValue(dataValue);
+    setIsSaveData(true);
   };
 
   return (
     <>
       <div className={styles.wrapper}>
-        {data.list.map((item, index) => (
-          <div className={styles.itemWrapper}>
-            <div className={styles.left}>
-              <div className={styles.label}>{item.label}</div>
-              <div className={styles.description}>{item.description}</div>
-            </div>
-            <div className={styles.right}>
-              {data.type === "edit" ? (
-                <EditType value={value[index]} type={item.type} />
-              ) : data.type === "image" ? (
-                <ImageType value={value[index]} />
-              ) : data.type === "enable" ? (
-                <EnableType value={value[index]} />
-              ) : (
-                ""
-              )}
-              <Button
-                color="gray"
-                onClick={
-                  data.type === "edit"
-                    ? () => handleEdit(index)
-                    : data.type === "image"
-                    ? () => handleChangeImage(index)
-                    : data.type === "enable"
-                    ? () => handleEnable(index)
-                    : ""
-                }
-              >
-                {data.type === "edit"
-                  ? "Edit"
-                  : data.type === "image"
-                  ? "Change"
-                  : data.type === "enable"
-                  ? "Enable"
-                  : ""}
-              </Button>
-            </div>
-            <EditPopup
-              show={show}
-              value={value[index]}
-              setValue={(data) => handleData(data, index)}
-              setShow={setShow}
-              type={item.type}
-              popup={item.popup}
-            />
+        <div className={styles.itemWrapper}>
+          <div className={styles.left}>
+            <div className={styles.label}>{data.label}</div>
+            <div className={styles.description}>{data.description}</div>
           </div>
-        ))}
+          <div className={styles.right}>
+            {data.type === "edit" ? (
+              <EditType value={data.value} type={data.type} />
+            ) : data.type === "image" ? (
+              <ImageType value={data.value} />
+            ) : data.type === "enable" ? (
+              <EnableType value={data.value} />
+            ) : (
+              ""
+            )}
+            <Button
+              color="gray"
+              onClick={
+                data.type === "edit"
+                  ? () => handleEdit()
+                  : data.type === "image"
+                  ? () => handleChangeImage()
+                  : data.type === "enable"
+                  ? () => handleEnable()
+                  : ""
+              }
+            >
+              {data.type === "edit"
+                ? "Edit"
+                : data.type === "image"
+                ? "Change"
+                : data.type === "enable"
+                ? "Enable"
+                : ""}
+            </Button>
+          </div>
+          <EditPopup
+            show={show}
+            value={data.value}
+            setValue={(editValue) => handleData(editValue)}
+            setShow={setShow}
+            type={data.type}
+            popup={data.popup}
+          />
+        </div>
       </div>
+
+      <CropDialog
+        open={cropDialogOpen}
+        file={data.file}
+        aspect={1}
+        onClose={() => setCropDialogOpen(false)}
+        onSave={(croppedImageData) => {
+          setCropDialogOpen(false);
+          data.setFile(dataURLtoFile(croppedImageData, data.file.name));
+          data.setValue(croppedImageData);
+          setIsSaveData(true);
+        }}
+      />
     </>
   );
 };
--- src/dashboardNew/containers/profileSettings/profileSettings.jsx
+++ src/dashboardNew/containers/profileSettings/profileSettings.jsx
@@ -1,106 +1,178 @@
+import { useContext, useEffect, useState } from "react";
 import Button from "../../components/button/button";
 import Card from "../../components/card/card";
 import SettingsItem from "../../components/settings/settingsItem";
 import SettingsTitle from "../../components/settings/settingsTitle";
 
 import styles from "./profileSettings.module.css";
-
-const data = [
-  {
-    list: [
-      {
-        label: "First name*",
-        description: "",
-        value: "Erin",
-      },
-      {
-        label: "Last name*",
-        description: "",
-        value: "Vaccaro",
-      },
-      {
-        label: "Business",
-        description:
-          "If you are a business, please enter your business name here.",
-        value: "",
-      },
-    ],
-    type: "edit",
-  },
-  {
-    list: [
-      {
-        label: "Email*",
-        description: "",
-        value: "er**@gmail.com",
-      },
-      {
-        label: "Phone number",
-        description: "",
-        value: "+38162**80",
-      },
-    ],
-    type: "edit",
-  },
-  {
-    list: [
-      {
-        label: "Avatar",
-        description: "Select an avatar to personalize your account. ",
-        value:
-          "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3161&q=80",
-      },
-    ],
-    type: "image",
-  },
-  {
-    list: [
-      {
-        label: "Marketing updates",
-        description:
-          "Receive marketing updates via email, push notifications (in the mobile app) and inbox notifications (in the web application).",
-        value: false,
-      },
-      {
-        label: "Email notifications",
-        description: "Receive notifications via email.",
-        value: true,
-      },
-      {
-        label: "App notifications",
-        description:
-          "Receive notifications via push notifications (in the mobile app).",
-        value: false,
-      },
-    ],
-    type: "enable",
-  },
-  {
-    list: [
-      {
-        label: "Notification language",
-        description:
-          "Select your preferred language for email, app push and on-site inbox notifications.",
-        value: "English",
-        popup: "language",
-      },
-    ],
-    type: "edit",
-  },
-  {
-    list: [
-      {
-        label: "Enable invoicing",
-        description:
-          "Receive invoices for each product sold for your accounting. ",
-        value: false,
-      },
-    ],
-    type: "enable",
-  },
-];
+import backend_API from "../../../api/backendAPI";
+import { MessageContext } from "../../../context/message";
 
 const ProfileSettings = () => {
+  const [firstName, setFirstName] = useState(localStorage.getItem("firstName"));
+  const [lastName, setLastName] = useState(localStorage.getItem("lastName"));
+  const [business, setBusiness] = useState(localStorage.getItem("business"));
+  const [phoneNumber, setPhoneNumber] = useState(
+    localStorage.getItem("phoneNumber"),
+  );
+  const [email, setEmail] = useState(localStorage.getItem("email"));
+  const [imageName, setImageName] = useState(null);
+  const [marketingUpdates, setMarketingUpdates] = useState(false);
+  const [emailNotification, setEmailNotification] = useState(false);
+  const [appNotification, setAppNotification] = useState(false);
+  const [notificationLanguage, setNotificationLanguage] = useState(false);
+  const [enableInvoicing, setEnableInvoicing] = useState(false);
+  const [isSaveData, setIsSaveData] = useState(false);
+  const [file, setFile] = useState(null);
+  const [imageChanged, setImageChanged] = useState(false);
+
+  const { setErrorMessage, setInfoMessage } = useContext(MessageContext);
+
+  const backendAPI = new backend_API();
+
+  const updateUser = async () => {
+    const requestData = {
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      email: email,
+      business: business || "",
+    };
+
+    const response = await backendAPI.update(requestData);
+    if (response == null) {
+      setErrorMessage("Error on updating data");
+      await backendAPI.signout();
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    }
+
+    if (response !== null) {
+      setInfoMessage("Settings updated successfully!");
+    }
+
+    setIsSaveData(false);
+  };
+
+  const uploadAvatar = async () => {
+    if (imageChanged) {
+      let resp2;
+      if (file) {
+        resp2 = await backendAPI.uploadFile(file);
+      } else {
+        resp2 = await backendAPI.deleteProfileImage(file);
+      }
+      if (resp2 == null) {
+        setErrorMessage("Error on uploading the profile picture");
+      }
+      setImageChanged(false);
+      setIsSaveData(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSaveData) {
+      if (!imageChanged) updateUser();
+      else uploadAvatar();
+    }
+  }, [isSaveData]);
+
+  const data = [
+    {
+      label: "First name*",
+      description: "",
+      value: firstName,
+      setValue: setFirstName,
+      type: "edit",
+    },
+    {
+      label: "Last name*",
+      description: "",
+      value: lastName,
+      setValue: setLastName,
+      type: "edit",
+    },
+    {
+      label: "Business",
+      description:
+        "If you are a business, please enter your business name here.",
+      value: business,
+      setValue: setBusiness,
+      type: "edit",
+    },
+    {
+      label: "Email*",
+      description: "",
+      value: email,
+      setValue: setEmail,
+      type: "edit",
+    },
+    {
+      label: "Phone number",
+      description: "",
+      value: phoneNumber,
+      setValue: setPhoneNumber,
+      type: "edit",
+    },
+    {
+      label: "Avatar",
+      description: "Select an avatar to personalize your account. ",
+      value: imageName,
+      setValue: setImageName,
+      type: "image",
+      file: file,
+      setFile: setFile,
+      imageChanged: imageChanged,
+      setImageChanged: setImageChanged,
+    },
+    {
+      label: "Marketing updates",
+      description:
+        "Receive marketing updates via email, push notifications (in the mobile app) and inbox notifications (in the web application).",
+      value: marketingUpdates,
+      setValue: setMarketingUpdates,
+      type: "enable",
+    },
+    {
+      label: "Email notifications",
+      description: "Receive notifications via email.",
+      value: emailNotification,
+      setValue: setEmailNotification,
+      type: "enable",
+    },
+    {
+      label: "App notifications",
+      description:
+        "Receive notifications via push notifications (in the mobile app).",
+      value: appNotification,
+      setValue: setAppNotification,
+      type: "enable",
+    },
+    {
+      label: "Notification language",
+      description:
+        "Select your preferred language for email, app push and on-site inbox notifications.",
+      value: notificationLanguage,
+      setValue: setNotificationLanguage,
+      popup: "language",
+      type: "edit",
+    },
+    {
+      label: "Enable invoicing",
+      description:
+        "Receive invoices for each product sold for your accounting. ",
+      value: enableInvoicing,
+      setValue: setEnableInvoicing,
+      type: "enable",
+    },
+  ];
+
+  useEffect(() => {
+    const profilePic = localStorage.getItem("profile_pic");
+    if (profilePic !== "null") setImageName(profilePic);
+  }, []);
+
   return (
     <Card className={styles.card}>
       <SettingsTitle
@@ -109,7 +181,7 @@ const ProfileSettings = () => {
       />
 
       {data.map((item) => (
-        <SettingsItem data={item} />
+        <SettingsItem data={item} setIsSaveData={setIsSaveData} />
       ))}
 
       {/*
