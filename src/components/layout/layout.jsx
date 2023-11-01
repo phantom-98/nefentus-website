7f0ade7512f60b107dbb89a20dd26aca3f96d0eb
Nazarii Melnyk <nazariimelnyk@outlook.com>
2023-11-01 12:05:16 +0200

    Go back to the version in the home page

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -56,54 +56,91 @@ const Layout = ({
   };
 
   return (
-    <div className="container">
+    <div
+      className={`container ${styles.layout} ${heading ? styles.hero : ""} ${
+        heading || load ? "load hero" : ""
+      }`}
+    >
       <div
-        className={`${styles.layout} ${heading ? styles.hero : ""} ${
-          heading || load ? "load hero" : ""
-        }`}
+        className={heading || load ? "" : "scroll"}
+        style={{ order: reverse ? 2 : 1 }}
       >
-        <div
-          className={heading || load ? "" : "scroll"}
-          style={{ order: reverse ? 2 : 1 }}
-        >
-          {subtitle && (
-            <p className={`${styles.subtitle} subtitle`}>{subtitle}</p>
-          )}
-          {heading && <h1>{heading}</h1>}
-          {title && <h3>{title}</h3>}
+        {subtitle && (
+          <p className={`${styles.subtitle} subtitle`}>{subtitle}</p>
+        )}
+        {heading && <h1>{heading}</h1>}
+        {title && <h3>{title}</h3>}
 
-          {description && (
+        {description && (
+          <div className={styles.descriptionWrapper}>
+            {home && <img src={Mega} alt="" />}
             <p
-              style={{ fontSize: home ? "2rem" : "" }}
+              style={{ fontSize: home ? "1.2rem" : "" }}
               className={`standard ${styles.description}`}
             >
               {description}
             </p>
-          )}
+          </div>
+        )}
 
-          {list && (
-            <div className={styles.list}>
-              <div>
-                <img src={Checkmark} alt="Checkmark" />
-                <p>{content[0]}</p>
+        {home && (
+          <>
+            <div className={styles.inputHero}>
+              <div className={styles.inputWrapper}>
+                <input type="text" placeholder="Email/Phone number" />
               </div>
-              <div>
-                <img src={Checkmark} alt="Checkmark" />
-                <p>{content[1]}</p>
+              {button && <Button link="/signup">{button}</Button>}
+            </div>
+            <div className={styles.connectWrapper}>
+              <div className={styles.connect}>
+                <p>Or Connect With</p>
+
+                <div className={styles.iconWrapper}>
+                  <div className={styles.iconBox}>
+                    <img src={Google} alt="" />
+                  </div>
+                  <div className={styles.iconBox}>
+                    <img src={Apple} alt="" />
+                  </div>
+                </div>
               </div>
-              <div>
-                <img src={Checkmark} alt="Checkmark" />
-                <p>{content[2]}</p>
-              </div>
-              <div>
-                <img src={Checkmark} alt="Checkmark" />
-                <p>{content[3]}</p>
+
+              <div className={`${styles.connect} ${styles.connect2}`}>
+                <p>App Download</p>
+
+                <div className={styles.iconWrapper}>
+                  <div className={styles.iconBox}>
+                    <img src={QR} alt="" />
+                  </div>
+                </div>
               </div>
             </div>
-          )}
+          </>
+        )}
 
-          <div className={styles.buttonWrapper}>
-            {/* {button && <Button link="/signup">{button}</Button>}
+        {list && (
+          <div className={styles.list}>
+            <div>
+              <img src={Checkmark} alt="Checkmark" />
+              <p>{content[0]}</p>
+            </div>
+            <div>
+              <img src={Checkmark} alt="Checkmark" />
+              <p>{content[1]}</p>
+            </div>
+            <div>
+              <img src={Checkmark} alt="Checkmark" />
+              <p>{content[2]}</p>
+            </div>
+            <div>
+              <img src={Checkmark} alt="Checkmark" />
+              <p>{content[3]}</p>
+            </div>
+          </div>
+        )}
+
+        <div className={styles.buttonWrapper}>
+          {/* {button && <Button link="/signup">{button}</Button>}
           {button2 && (
             <div className={styles.button}>
               <p>{button2}</p>
@@ -115,99 +152,63 @@ const Layout = ({
             </div>
           )} */}
 
-            {store && (
-              <div className={styles.store}>
-                <p className="standard">{t("home.heroAvailable")}</p>
+          {store && (
+            <div className={styles.store}>
+              <p className="standard">{t("home.heroAvailable")}</p>
 
-                <div className={styles.buttonWrapperMob}>
-                  <Button link="/" color="white">
-                    <img src={Android} alt="android logo" />
-                    <span>Play Store</span>
-                  </Button>
-                  <Button link="/" color="white">
-                    <img src={Apple} alt="apple logo" />
-                    <span>App Store</span>
-                  </Button>
-                </div>
+              <div className={styles.buttonWrapperMob}>
+                <Button link="/" color="white">
+                  <img src={Android} alt="android logo" />
+                  <span>Play Store</span>
+                </Button>
+                <Button link="/" color="white">
+                  <img src={Apple} alt="apple logo" />
+                  <span>App Store</span>
+                </Button>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
-        {!video && !children && (
-          <img
-            className={`${
-              heading || load ? "" : reverse ? "slide-right" : "slide-left"
-            } ${full ? styles.full : ""}`}
-            src={image}
-            alt="nefentus graphics"
-            style={{ order: reverse ? 1 : 2 }}
-          />
-        )}
-
-        {video && !children && (
-          <video
-            onLoadedData={handleLoad}
-            ref={videoRef}
-            style={{ order: reverse ? 1 : 2 }}
-            className={heading ? "" : ""}
-            controls={false}
-            autoPlay
-            playsInline
-            muted
-            loop
-          >
-            <source src={video} type="video/mp4" />
-          </video>
-        )}
-
-        {children && (
-          <div
-            className={`${
-              heading || load
-                ? "load hero"
-                : reverse
-                ? "slide-right"
-                : "slide-left"
-            } ${full ? styles.full : ""}`}
-            style={{ order: reverse ? 1 : 2 }}
-          >
-            {children}
-          </div>
-        )}
       </div>
+      {!video && !children && (
+        <img
+          className={`${
+            heading || load ? "" : reverse ? "slide-right" : "slide-left"
+          } ${full ? styles.full : ""}`}
+          src={image}
+          alt="nefentus graphics"
+          style={{ order: reverse ? 1 : 2 }}
+        />
+      )}
 
-      {home && (
-        <div className={`${styles.layout}`}>
-          <div className={styles.inputHero}>
-            <div className={styles.inputWrapper}>
-              <input type="text" placeholder="Email" />
-            </div>
-            {button && <Button link="/signup">{button}</Button>}
-          </div>
-          <div className={styles.connectWrapper}>
-            <div className={styles.connect}>
-              <p>Or Connect With</p>
+      {video && !children && (
+        <video
+          onLoadedData={handleLoad}
+          ref={videoRef}
+          style={{ order: reverse ? 1 : 2 }}
+          className={heading ? "" : ""}
+          controls={false}
+          autoPlay
+          playsInline
+          muted
+          loop
+        >
+          <source src={video} type="video/mp4" />
+        </video>
+      )}
 
-              <div className={styles.iconWrapper}>
-                <div className={styles.iconBox}>
-                  <img src={Google} alt="" />
-                </div>
-                <div className={styles.iconBox}>
-                  <img src={Apple} alt="" />
-                </div>
-              </div>
-            </div>
-
-            <div className={`${styles.connect} ${styles.connect2}`}>
-              <p>App Download</p>
-
-              <div className={styles.iconWrapper}>
-                <div className={styles.iconBox}>
-                  <img src={QR} alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
+      {children && (
+        <div
+          className={`${
+            heading || load
+              ? "load hero"
+              : reverse
+              ? "slide-right"
+              : "slide-left"
+          } ${full ? styles.full : ""}`}
+          style={{ order: reverse ? 1 : 2 }}
+        >
+          {children}
         </div>
       )}
     </div>
