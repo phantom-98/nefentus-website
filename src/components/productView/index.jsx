9a5f9334ccbcf995e4a4e79c56d44ae160307627
PavloPosternak <posternakpavlo98@gmail.com>
2024-01-22 14:13:44 +1000

    DEV-68 product view design

--- src/components/productView/index.jsx
+++ src/components/productView/index.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../button/button";
 import styles from "./productView.module.css";
 import backendAPI from "../../api/backendAPI";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const ProductView = ({ product }) => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const [pic, setPic] = useState();
+  const [more, setMore] = useState(false);
+  const [show, setShow] = useState(false);
 
   const getProductImage = async () => {
     const res = await new backendAPI().getProductImage(product.link);
@@ -18,49 +22,65 @@ const ProductView = ({ product }) => {
     getProductImage();
   }, [product]);
 
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      const current = divRef.current;
+      setShow(current.scrollHeight > 250);
+    }
+  }, []);
+
   return (
     <div className={styles.productWrapper}>
-      <p>Steven Products/ Rolex Watch</p>
       <div className={styles.wrapper}>
         <div className={styles.imageWrapper}>
-          <img className={styles.image} src={pic} alt="Product Image" />
+          <img src={pic} alt="Product Image" />
         </div>
         <div className={styles.infoWrapper}>
-          <h1 className={styles.name}>{product.name}</h1>
+          <h1>{product.name}</h1>
           <div className={styles.descriptionWrapper}>
-            <div className={styles.descriptionHeaderWrapper}>
-              <p className={styles.descriptionHeader}>Product Description</p>
+            <div className={styles.descriptionHeader}>
+              <p>{t("products.view.description")}</p>
             </div>
-            <div className={styles.descriptionBodyWrapper}>
-              {product.description &&
-                product.description.split("\n").map((item) => {
-                  return <p className={styles.descriptionBody}>{item}</p>;
-                })}
-              <p className={styles.descriptionBody}>Show less</p>
+            <div className={styles.descriptionBody}>
+              <div className={more ? "" : styles.less} ref={divRef}>
+                {product.description &&
+                  product.description.split("\n").map((item) => {
+                    return <p>{item}</p>;
+                  })}
+              </div>
+              {show && (
+                <p onClick={() => setMore((prev) => !prev)}>
+                  {more
+                    ? t("products.view.showLess")
+                    : t("products.view.showMore")}
+                </p>
+              )}
             </div>
           </div>
           {product.user && (
-            <div className={styles.sellerWrapper}>
-              <p>Seller: </p>
+            <div className={styles.seller}>
+              <p>{t("products.view.seller")}: </p>
               <p>{product.user.firstName + " " + product.user.lastName}</p>
             </div>
           )}
           <div className={styles.actionWrapper}>
             <div className={styles.priceWrapper}>
-              <p>Unit Price</p>
-              <p>{product.price}</p>
+              <p>{t("products.view.price")}</p>
+              <p>${product.price}</p>
             </div>
             <div className={styles.stockWrapper}>
-              <p>Stock Quantity</p>
+              <p>{t("products.view.stock")}</p>
               <p>{product.stock}</p>
             </div>
             <Button
-              style={{ width: "40%", height: "55px" }}
+              style={{ width: "30%", height: "55px" }}
               onClick={() => {
                 navigate(`/product/${product.link}/pay`);
               }}
             >
-              Buy
+              {t("products.view.buy")}
             </Button>
           </div>
         </div>
