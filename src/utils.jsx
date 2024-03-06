f5533c73d9188f7dc2cfcef1797aa53d319aa2c3
PavloPosternak <posternakpavlo98@gmail.com>
2024-03-06 23:19:22 +0200

    dev 169 simple regex of phone number

--- src/utils.jsx
+++ src/utils.jsx
@@ -129,7 +129,6 @@ export const validateEmail = (email) => {
 };
 
 export const validatePhoneNumber = (number) => {
-  const phoneRegex =
-    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+  const phoneRegex = /[\+\-\/\\\(\)0-9]*/;
   return phoneRegex.test(number);
 };
