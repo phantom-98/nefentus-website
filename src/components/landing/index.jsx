1afc1fe4eab1ab7db8fb38a6fea3d930e4595852
tahakhanabdalli <tahakhanabdalli@gmail.com>
2024-11-07 16:01:43 +0500

    Removed background from container in contact us page

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -329,7 +329,6 @@ const Feature = ({
         className="feature-header title"
         onClick={() => {
           setExpands &&
-            !expands[index] &&
             index !== undefined &&
             setExpands(
               expands.map((item, i) => {
